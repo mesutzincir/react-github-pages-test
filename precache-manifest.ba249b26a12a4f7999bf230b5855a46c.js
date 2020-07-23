867c7ca1af4aac6429e82dbf93a6af5d04f6171b
mesut zincir <mesutzincir@gmail.com>
2020-07-23 23:07:14 +0100

    Updates

---
+++ precache-manifest.ba249b26a12a4f7999bf230b5855a46c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4052ae3777243a2276c14afe1510c2d1",
+    "url": "/react-github-pages-test/index.html"
+  },
+  {
+    "revision": "743eeef9f73cdba6c286",
+    "url": "/react-github-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c0fc2053487a82adfdaf",
+    "url": "/react-github-pages-test/static/js/2.b3bc25d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-pages-test/static/js/2.b3bc25d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743eeef9f73cdba6c286",
+    "url": "/react-github-pages-test/static/js/main.1450b22e.chunk.js"
+  },
+  {
+    "revision": "56024eae99ad3d0936d3",
+    "url": "/react-github-pages-test/static/js/runtime-main.79b2206e.js"
+  }
+]);
